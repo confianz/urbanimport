e14000e2dcf97df8059fa2e482e803ea5052ad36
shibi <shibi.thambi@confianzit.com>
2021-09-07 17:39:54 +0530

    dependency fix

--- warehouse_selection_fedex/__manifest__.py
+++ warehouse_selection_fedex/__manifest__.py
@@ -19,7 +19,7 @@ Warehouse Selection with Address Verification
     'init_xml': [
     ],
 
-    'depends': ['delivery', 'delivery_fedex','stock', 'shipstation_shipping', 'sale_extension', 'sale_amazon_extension'],
+    'depends': ['delivery', 'delivery_fedex','stock', 'shipstation_shipping', 'sale_extension'],
     'installable': True,
     'auto_install': False,
     'application': False,
